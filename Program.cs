3d9fc8f9835d9c910c0c80a2124fab272ee634bf
themysterycoderuk <55763524+themysterycoderuk@users.noreply.github.com>
2020-02-07 16:27:21 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DateChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private void showDates()
+        {
+            var dt1 = new DateTime(2020, 12, 5);
+            var dt2 = new DateTime(2020, 4, 5);
+
+            Console.WriteLine($"5/12/2020 = {dt1.ToString()} (default), {dt1.ToString("dd/MM/yyyy")} (dd/MM/yyyy)");
+            Console.WriteLine($"5/12/2020 = {dt2.ToString()} (default), {dt2.ToString("dd/MM/yyyy")} (dd/MM/yyyy)");
+        }
+    }
+}
