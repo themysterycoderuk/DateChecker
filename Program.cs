082e1412c09abcbebcf10655544e9e1fe7877ef7
themysterycoderuk <55763524+themysterycoderuk@users.noreply.github.com>
2020-02-07 17:00:53 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -14,9 +14,11 @@ namespace DateChecker
         {
             var dt1 = new DateTime(2020, 12, 5);
             var dt2 = new DateTime(2020, 4, 5);
+            var dt3 = DateTime.Parse("05/04/2020");
 
-            Console.WriteLine($"5/12/2020 = {dt1.ToString()} (default), {dt1.ToString("dd/MM/yyyy")} (dd/MM/yyyy)");
-            Console.WriteLine($"5/12/2020 = {dt2.ToString()} (default), {dt2.ToString("dd/MM/yyyy")} (dd/MM/yyyy)");
+            Console.WriteLine($"Date 5/12/2020 = {dt1.ToString()} (default), {dt1.ToString("dd/MM/yyyy")} (dd/MM/yyyy)");
+            Console.WriteLine($"Date 5/4/2020 = {dt2.ToString()} (default), {dt2.ToString("dd/MM/yyyy")} (dd/MM/yyyy)");
+            Console.WriteLine($"String 05/04/2020 = {dt3.ToString()} (default), {dt3.ToString("dd/MM/yyyy")} (dd/MM/yyyy)");
         }
     }
 }
